5fbd38bc6b5132fb06c5c9cef7b2f980dbb8e140
SurajSharma <88067668+Asthraj@users.noreply.github.com>
2026-04-14 11:21:21 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-<<<<<<< HEAD
+
 import cloudinary
 
 cloudinary.config(
@@ -133,5 +133,4 @@ cloudinary.config(
     api_secret='1h61Rc0A8IIO_bTOK-evctUkOTw'
 )
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-=======
->>>>>>>
+
